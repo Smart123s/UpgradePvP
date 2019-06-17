7b4dc01d5c72bbb7eb42724a234ca9175a04154f
Smart123s <28480228+Smart123s@users.noreply.github.com>
2019-06-17 15:39:58 +0200

    Shortened ShopInventoryListener.java

--- src/upgradepvp/shop/ShopInventory.java
+++ src/upgradepvp/shop/ShopInventory.java
@@ -16,87 +16,84 @@ import upgradepvp.economy.ConfigLocations;
 import upgradepvp.main.ConfigManager;
 
 public class ShopInventory{
-	ConfigManager config = ConfigManager.getInstance();	
+	static ConfigManager config = ConfigManager.getInstance();	
 	
 	public final static Inventory start = generateStartInv();
 	public final static Inventory swords = generateSwordsInv();
 	public final static Inventory armor = generateArmorInv();
 	public final static Inventory ench = generateEnchInv();
 	public final static Inventory other = generateOtherInv();
+	public final static ItemStack goBackItem = createGoBackItem();
+	
 	
 	private static Inventory generateStartInv() {
 		Inventory inv = Bukkit.createInventory(null, 9, "UpgradePvp Shop > Main");
-		ShopInventory shopInv = new ShopInventory();
 		
-		inv.setItem(0, shopInv.createCategoryItem(Material.WOOD_SWORD, "Swords"));
-		inv.setItem(1, shopInv.createCategoryItem(Material.CHAINMAIL_CHESTPLATE, "Armor"));
-		inv.setItem(2, shopInv.createCategoryItem(Material.ENCHANTED_BOOK, "Enchantments"));
-		inv.setItem(3, shopInv.createCategoryItem(Material.FISHING_ROD, "Other"));
+		inv.setItem(0, createCategoryItem(Material.WOOD_SWORD, "Swords"));
+		inv.setItem(1, createCategoryItem(Material.CHAINMAIL_CHESTPLATE, "Armor"));
+		inv.setItem(2, createCategoryItem(Material.ENCHANTED_BOOK, "Enchantments"));
+		inv.setItem(3, createCategoryItem(Material.FISHING_ROD, "Other"));
 
-		inv.setItem(7, shopInv.createFinishMyGame());
+		inv.setItem(7, createFinishMyGame());
 		
-		inv.setItem(8, shopInv.createGoBackItem());
+		inv.setItem(8, createGoBackItem());
 		
-		shopInv=null;
 		return inv;
 	}
 	
 	private static Inventory generateSwordsInv() {
 		Inventory inv = Bukkit.createInventory(null, 9, "UpgradePvp Shop > Swords");
-		ShopInventory shopInv = new ShopInventory();
+		new ShopInventory();
 		
-		inv.setItem(8, shopInv.createGoBackItem());
+		inv.setItem(8, createGoBackItem());
 		
-		inv.setItem(0, shopInv.createBasicItem(Material.WOOD_SWORD));
-		inv.setItem(1, shopInv.createBasicItem(Material.GOLD_SWORD));
-		inv.setItem(2, shopInv.createBasicItem(Material.STONE_SWORD));
-		inv.setItem(3, shopInv.createBasicItem(Material.IRON_SWORD));
-		inv.setItem(4, shopInv.createBasicItem(Material.DIAMOND_SWORD));
+		inv.setItem(0, createBasicItem(Material.WOOD_SWORD));
+		inv.setItem(1, createBasicItem(Material.GOLD_SWORD));
+		inv.setItem(2, createBasicItem(Material.STONE_SWORD));
+		inv.setItem(3, createBasicItem(Material.IRON_SWORD));
+		inv.setItem(4, createBasicItem(Material.DIAMOND_SWORD));
 		
-		shopInv=null;
 		return inv;
 	}
 	
 	private static Inventory generateArmorInv() {
 		Inventory inv = Bukkit.createInventory(null, 9*3, "UpgradePvp Shop > Armor");
-		ShopInventory shopInv = new ShopInventory();
+		new ShopInventory();
 		
-		inv.setItem(26, shopInv.createGoBackItem());
+		inv.setItem(26, createGoBackItem());
 		
-		inv.setItem(0, shopInv.createBasicItem(Material.LEATHER_BOOTS));
-		inv.setItem(1, shopInv.createBasicItem(Material.LEATHER_LEGGINGS));
-		inv.setItem(2, shopInv.createBasicItem(Material.LEATHER_CHESTPLATE));
-		inv.setItem(3, shopInv.createBasicItem(Material.LEATHER_HELMET));
+		inv.setItem(0, createBasicItem(Material.LEATHER_BOOTS));
+		inv.setItem(1, createBasicItem(Material.LEATHER_LEGGINGS));
+		inv.setItem(2, createBasicItem(Material.LEATHER_CHESTPLATE));
+		inv.setItem(3, createBasicItem(Material.LEATHER_HELMET));
 		
-		inv.setItem(5, shopInv.createBasicItem(Material.CHAINMAIL_BOOTS));
-		inv.setItem(6, shopInv.createBasicItem(Material.CHAINMAIL_LEGGINGS));
-		inv.setItem(7, shopInv.createBasicItem(Material.CHAINMAIL_CHESTPLATE));
-		inv.setItem(8, shopInv.createBasicItem(Material.CHAINMAIL_HELMET));
+		inv.setItem(5, createBasicItem(Material.CHAINMAIL_BOOTS));
+		inv.setItem(6, createBasicItem(Material.CHAINMAIL_LEGGINGS));
+		inv.setItem(7, createBasicItem(Material.CHAINMAIL_CHESTPLATE));
+		inv.setItem(8, createBasicItem(Material.CHAINMAIL_HELMET));
 		
-		inv.setItem(9, shopInv.createBasicItem(Material.GOLD_BOOTS));
-		inv.setItem(10, shopInv.createBasicItem(Material.GOLD_LEGGINGS));
-		inv.setItem(11, shopInv.createBasicItem(Material.GOLD_CHESTPLATE));
-		inv.setItem(12, shopInv.createBasicItem(Material.GOLD_HELMET));
+		inv.setItem(9, createBasicItem(Material.GOLD_BOOTS));
+		inv.setItem(10, createBasicItem(Material.GOLD_LEGGINGS));
+		inv.setItem(11, createBasicItem(Material.GOLD_CHESTPLATE));
+		inv.setItem(12, createBasicItem(Material.GOLD_HELMET));
 		
-		inv.setItem(14, shopInv.createBasicItem(Material.IRON_BOOTS));
-		inv.setItem(15, shopInv.createBasicItem(Material.IRON_LEGGINGS));
-		inv.setItem(16, shopInv.createBasicItem(Material.IRON_CHESTPLATE));
-		inv.setItem(17, shopInv.createBasicItem(Material.IRON_HELMET));
+		inv.setItem(14, createBasicItem(Material.IRON_BOOTS));
+		inv.setItem(15, createBasicItem(Material.IRON_LEGGINGS));
+		inv.setItem(16, createBasicItem(Material.IRON_CHESTPLATE));
+		inv.setItem(17, createBasicItem(Material.IRON_HELMET));
 		
-		inv.setItem(18, shopInv.createBasicItem(Material.DIAMOND_BOOTS));
-		inv.setItem(19, shopInv.createBasicItem(Material.DIAMOND_LEGGINGS));
-		inv.setItem(20, shopInv.createBasicItem(Material.DIAMOND_CHESTPLATE));
-		inv.setItem(21, shopInv.createBasicItem(Material.DIAMOND_HELMET));
+		inv.setItem(18, createBasicItem(Material.DIAMOND_BOOTS));
+		inv.setItem(19, createBasicItem(Material.DIAMOND_LEGGINGS));
+		inv.setItem(20, createBasicItem(Material.DIAMOND_CHESTPLATE));
+		inv.setItem(21, createBasicItem(Material.DIAMOND_HELMET));
 		
-		shopInv=null;
 		return inv;
 	}
 	
 	private static Inventory generateEnchInv() {
 		Inventory inv = Bukkit.createInventory(null, 9*2, "UpgradePvp Shop > Enchantments");
-		ShopInventory shopInv = new ShopInventory();
 		
-		inv.setItem(17, shopInv.createGoBackItem());
+		inv.setItem(17, createGoBackItem());
 		
 		inv.setItem(0, createEnchantedBook("Unbreaking"));
 		inv.setItem(1, createEnchantedBook("Protection"));
@@ -115,30 +112,28 @@ public class ShopInventory{
 		inv.setItem(14, createEnchantedBook("Flame"));
 		inv.setItem(15, createEnchantedBook("Infinity"));
 		
-		shopInv=null;
 		return inv;
 	}
 	
 	private static Inventory generateOtherInv() {
 		Inventory inv = Bukkit.createInventory(null, 9, "UpgradePvp Shop > Others");
-		ShopInventory shopInv = new ShopInventory();
+		new ShopInventory();
 		
-		inv.setItem(8, shopInv.createGoBackItem());
+		inv.setItem(8, createGoBackItem());
 		
-		inv.setItem(0, shopInv.createBasicItem(Material.BOW));
-		inv.setItem(1, shopInv.createBasicItem(Material.ARROW));
-		inv.setItem(2, shopInv.createBasicItem(Material.FISHING_ROD));
-		inv.setItem(3, shopInv.createBasicItem(Material.SNOW_BALL));
+		inv.setItem(0, createBasicItem(Material.BOW));
+		inv.setItem(1, createBasicItem(Material.ARROW));
+		inv.setItem(2, createBasicItem(Material.FISHING_ROD));
+		inv.setItem(3, createBasicItem(Material.SNOW_BALL));
 		
 		inv.setItem(4, generateKeepInvItem());
 	
-		shopInv=null;
 		return inv;
 	}
 	
 	private static ItemStack generateKeepInvItem() {
-		ShopInventory shopInv = new ShopInventory();
-		ItemStack keepInvItem = shopInv.createItemWithName(Material.EXP_BOTTLE, "Other.KeepInventory",ChatColor.AQUA + "Keep Inventory");
+		new ShopInventory();
+		ItemStack keepInvItem = createItemWithName(Material.EXP_BOTTLE, "Other.KeepInventory",ChatColor.AQUA + "Keep Inventory");
 		List<String> lore = keepInvItem.getItemMeta().getLore();
 		lore.add(ChatColor.BLUE + "" + ChatColor.ITALIC + "Permament");
 		ItemMeta meta = keepInvItem.getItemMeta();
@@ -147,7 +142,7 @@ public class ShopInventory{
 		return keepInvItem;
 	}
 
-	private ItemStack createBasicItem(Material material) {
+	private static ItemStack createBasicItem(Material material) {
 		ItemStack item = new ItemStack(material, 1);
 		ItemMeta meta = item.getItemMeta();
 		List<String> lore = new ArrayList<String>();
@@ -157,7 +152,7 @@ public class ShopInventory{
 		return item;		
 	}
 	
-	private ItemStack createGoBackItem() {
+	private static ItemStack createGoBackItem() {
 		ItemStack item = new ItemStack(Material.BARRIER, 1);
 		ItemMeta meta = item.getItemMeta();
 		item.setType(Material.BARRIER);
@@ -166,7 +161,7 @@ public class ShopInventory{
 		return item;
 	}
 	
-	private ItemStack createItemWithName(Material material, String priceLoc, String name) {
+	private static ItemStack createItemWithName(Material material, String priceLoc, String name) {
 		ItemStack item = new ItemStack(material, 1);
 		ItemMeta meta = item.getItemMeta();
 		List<String> lore = new ArrayList<String>();
@@ -177,7 +172,7 @@ public class ShopInventory{
 		return item;
 	}
 	
-	private ItemStack createCategoryItem(Material material, String name) {
+	private static ItemStack createCategoryItem(Material material, String name) {
 		ItemStack item = new ItemStack(material, 1);
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(name);
@@ -188,7 +183,7 @@ public class ShopInventory{
 		return item;
 	}
 	
-	private ItemStack createFinishMyGame() {
+	private static ItemStack createFinishMyGame() {
 		ItemStack item = new ItemStack(Material.DIAMOND, 1);
 		ItemMeta meta = item.getItemMeta();
 		List<String> lore = new ArrayList<String>();
@@ -202,8 +197,8 @@ public class ShopInventory{
 	}
 	
 	private static ItemStack createEnchantedBook(String ench) {
-		ShopInventory shopInv = new ShopInventory();
+		new ShopInventory();
 		final String priceLoc = ConfigLocations.getEnchLoc(ConfigLocations.getBookName(ench));
-		return shopInv.createItemWithName(Material.ENCHANTED_BOOK, priceLoc, ChatColor.AQUA + ench);
+		return createItemWithName(Material.ENCHANTED_BOOK, priceLoc, ChatColor.AQUA + ench);
 	}
 }
