69b2e0af8c2425746a254459096b8e97081afd69
AtorVP64 <52372975+AtorVP64@users.noreply.github.com>
2019-06-30 18:39:21 +0200

    Modified Calculation (AtorVP64)

--- src/upgradepvp/economy/Rewarding.java
+++ src/upgradepvp/economy/Rewarding.java
@@ -28,9 +28,9 @@ public class Rewarding {
 		if (deadEco.hasKeepInv()) deadEco.storeKeepInvItems();
 		//TODO:Improve Calculation
 		//Calculate the award of the murder
-		int murderAward = (int) Math.round((((float)(CalcInvValue.getInvVal(dead)+1)/(float)(CalcInvValue.getInvVal(murder)+1)*0.2)*(deadEco.getCommonMoney() + deadEco.getSafeMoney() + CalcInvValue.getInvVal(dead))));
+		int murderAward = (int) Math.round(deadEco.getCommonMoney() + deadEco.getSafeMoney() + CalcInvVal.getInvVal(dead) - CalcInvVal.getInvVal(murder))*0.5);
 		//Calculate the remaining money of the dead person
-		int deadAward = (int) Math.round((deadEco.getCommonMoney() + murderEco.getCommonMoney() + murderEco.getSafeMoney() + CalcInvValue.getInvVal(murder))*0.4);
+		int deadAward = (int) Math.round((murderEco.getCommonMoney() + murderEco.getSafeMoney() + CalcInvValue.getInvVal(murder) - CalcInvValue.getInvVal(dead))*0.4);
 		//If the dead player doesen't have keepinv, give them more money
 		if (!deadEco.hasKeepInv()) deadAward += (int) (Math.round(CalcInvValue.getInvVal(dead)*0.25));
 		//If the remaining money of the dead player is below the startingMoney, give them the startingMoney
