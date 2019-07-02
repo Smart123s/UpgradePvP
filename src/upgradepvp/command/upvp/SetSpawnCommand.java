c62f7c8ebbe165927ce832849c8c874577826e09
Smart123s <28480228+Smart123s@users.noreply.github.com>
2019-07-02 12:53:56 +0200

    Started UPvP Command

---
+++ src/upgradepvp/command/upvp/SetSpawnCommand.java
@@ -0,0 +1,23 @@
+package upgradepvp.command.upvp;
+
+import org.bukkit.entity.Player;
+
+import upgradepvp.main.Main;
+import upgradepvp.map.UPvPMap;
+
+public class SetSpawnCommand {
+
+	public static void run(String[] args, Player player) {
+		if (args.length == 2) {
+			if (!UPvPMap.exists(args[1])) {
+				player.sendMessage(Main.prefixError + "Map " + args[1] + " does not exist!");
+				return;
+			}
+			UPvPMap.get(args[1]).setSpawn(player.getLocation());
+			player.sendMessage(Main.prefix + "Successfully set the spawn of map " + args[1]);
+		} else {
+			player.sendMessage(Main.prefixError + "/UPvP setspawn <map>");
+		}
+	}
+	
+}
---
+++ src/upgradepvp/command/upvp/StartCommand.java
@@ -0,0 +1,22 @@
+package upgradepvp.command.upvp;
+
+import org.bukkit.entity.Player;
+
+import upgradepvp.main.Main;
+import upgradepvp.map.UPvPMap;
+
+public class StartCommand {
+
+	public static void run(String[] args, Player player) {
+		if (args.length == 2) {
+			if (!UPvPMap.exists(args[1])) {
+				player.sendMessage(Main.prefixError + "Map " + args[1] + " does not exist!");
+				return;
+			}
+			UPvPMap.get(args[1]).startGame();
+		} else {
+			player.sendMessage(Main.prefixError + "/UPvP start <map>");
+		}
+	}
+	
+}
